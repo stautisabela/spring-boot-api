d0d85083edfa2591fc2639d4d85b005954a579d5
stautisabela <stautisabela@gmail.com>
2023-01-03 08:08:26 +0100

    first commit

---
+++ complete-spring-boot-api/src/main/java/com/stautisabela/completespringbootapi/CompleteSpringBootApiApplication.java
@@ -0,0 +1,13 @@
+package com.stautisabela.completespringbootapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompleteSpringBootApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompleteSpringBootApiApplication.class, args);
+	}
+
+}
---
+++ complete-spring-boot-api/src/test/java/com/stautisabela/completespringbootapi/CompleteSpringBootApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stautisabela.completespringbootapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompleteSpringBootApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
