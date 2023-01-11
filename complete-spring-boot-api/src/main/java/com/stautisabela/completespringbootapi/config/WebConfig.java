59c4c82f8c0d6c11dcf3571159fc80a4fd24d455
stautisabela <stautisabela@gmail.com>
2023-01-11 15:13:34 +0100

    implemented content negociation via header

--- complete-spring-boot-api/src/main/java/com/stautisabela/completespringbootapi/config/WebConfig.java
+++ complete-spring-boot-api/src/main/java/com/stautisabela/completespringbootapi/config/WebConfig.java
@@ -8,16 +8,29 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-	// allows get methods in json and xml formats via query params: ?mediaType=json or ?mediaType=xml
+	// allows get methods in json and xml formats
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
 
+	/*
+	 * Via query parameters: ?mediaType=json or ?mediaType=xml
+	 * 
 		configurer.favorParameter(true)
 				  .parameterName("mediaType")
 				  .ignoreAcceptHeader(true)
 				  .useRegisteredExtensionsOnly(false)
 				  .defaultContentType(MediaType.APPLICATION_JSON)
 				  .mediaType("json", MediaType.APPLICATION_JSON)
-				  .mediaType("xml", MediaType.APPLICATION_XML);
+				  .mediaType("xml", MediaType.APPLICATION_XML);	 
+	*/
+
+	// Via header parameters: key = Accept, value = application/xml
+		
+		configurer.favorParameter(false)
+		  .ignoreAcceptHeader(false)
+		  .useRegisteredExtensionsOnly(false)
+		  .defaultContentType(MediaType.APPLICATION_JSON)
+		  .mediaType("json", MediaType.APPLICATION_JSON)
+		  .mediaType("xml", MediaType.APPLICATION_XML);	 
 	}
 }
